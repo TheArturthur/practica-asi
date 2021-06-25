37d6903ce127b8e07be4953c21762e9561c3aebd
Arturo Vidal Peña <arturo.vidal.96@gmail.com>
2021-06-25 19:28:22 +0200

    CU-y320tz Final changes to project.

--- memoria/build.sh
+++ memoria/build.sh
@@ -2,7 +2,7 @@
 
 git log --no-merges --encoding="UTF-8" --reverse --date=format:"%a, %e de %B" --pretty=format:"\item \textbf{\underline{\underline{Fecha:} %ad, \underline{Autor:} %an}}\\\\\item[] %s\\\\" > includes/log.tex
 
-sed -ir -e 's/\&/\\\&/g' -e 's/\ \+[[:digit:]]\{4\}//g' -e 's/ CU-y320tz//g' -e 's/Pérez Martín/Perez Martin/g' -e 's/TheArturthur/Arturo Vidal Peña/g' -e 's/asudara/Alfonso Sudara Padilla/g' includes/log.tex
+sed -ir -e 's/\&/\\\&/g' -e 's/\ \+[[:digit:]]\{4\}//g' -e 's/ CU-[[:alnum:]]//g' -e 's/Pérez Martín/Perez Martin/g' -e 's/TheArturthur/Arturo Vidal Peña/g' -e 's/asudara/Alfonso Sudara Padilla/g' includes/log.tex
 rm includes/log.texr
 
 latexmk -c -pdf -pdflatex="pdflatex -interaction=nonstopmode" -use-make practica_asi.tex
