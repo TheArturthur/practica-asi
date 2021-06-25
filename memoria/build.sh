7531ae25f959eb07279dc18b59336b4135be95cb
Arturo Vidal Peña <arturo.vidal.96@gmail.com>
2021-06-25 15:44:01 +0200

    CU-y320tz Removed commits sha from output.

--- memoria/build.sh
+++ memoria/build.sh
@@ -4,7 +4,7 @@ tree -n -I vm -o secciones/estructura_ficheros.tex ..
 
 sed -ir -e '/^$/d' -e 's/^\.\..*/\\begin\{verbatim\}/' -e 's/^[[:digit:]][[:digit:]]*.*/\\end\{verbatim\}/g' -e 's/[├│└]/\|/g' -e 's/──/--/g' secciones/estructura_ficheros.tex
 
-git log --no-merges --encoding="UTF-8" --reverse --date=format:"%a, %e de %B" --pretty=format:"\item \textbf{\underline{\textit{Commit:}} \#%h, \underline{Fecha:} %ad, \underline{Autor:} %an}\\\\\item[] %s\\\\" > includes/log.tex
+git log --no-merges --encoding="UTF-8" --reverse --date=format:"%a, %e de %B" --pretty=format:"\item \textbf{\underline{\underline{Fecha:} %ad, \underline{Autor:} %an}\\\\\item[] %s\\\\" > includes/log.tex
 
 sed -ir -e 's/\&/\\\&/g' -e 's/\ \+[[:digit:]]\{4\}//g' -e 's/ CU-y320tz//g' -e 's/Pérez Martín/Perez Martin/g' -e 's/TheArturthur/Arturo Vidal Peña/g' -e 's/asudara/Alfonso Sudara Padilla/g' includes/log.tex
 
